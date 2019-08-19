463b2fd50471a009eee25c941c7f96bc22539e5b
Ahmer Latif Khan <54271020+Ahmer-khan@users.noreply.github.com>
2019-08-19 16:44:48 +0500

    Update influential_user_followers_ids.py

--- influential_user_followers_ids.py
+++ influential_user_followers_ids.py
@@ -1,13 +1,13 @@
 from twython import Twython, TwythonRateLimitError, TwythonError
 import os,time
 import pandas as pd
-API_Key = "WssmC5VHt7Tl5aIWGVOZKRXHp"
-API_Secret = "GXWYXzs2bVB1RORpVvPNHpzJePh9gjccYqKbrheYys2zCHidcx"
-Access_Token = "746410136831299584-rrJZvVr5s3vzrCYsZ2qJvqLc94hGMOp"
-Access_Token_Secret = "yfQyB51CVF7LMaSD2x3AcBS155YLm0SiF6rF2bBIs9WpJ"
+API_Key = ""
+API_Secret = ""
+Access_Token = ""
+Access_Token_Secret = ""
 twitter_final = Twython(API_Key, API_Secret,
                   Access_Token, Access_Token_Secret)
-influential_user_list = pd.read_csv('influential_user_list.csv',sep=',')
+influential_user_list = pd.read_csv(#path)
 influential_user_list = influential_user_list[0:5]
 for index,row in influential_user_list.iterrows():
     row['name'] = row['name'].replace("b'","")
@@ -15,7 +15,7 @@ for index,row in influential_user_list.iterrows():
     row['name'] = row['name'].replace(" ","")
     row['user_name'] = row['user_name'].replace("b'@","")
     row['user_name'] = row['user_name'].replace("'","")
-    if not os.path.isfile('influential_user_follower/'+ str(row['Rank']) + "_" + row['name']+'/folloers_ids.txt') :
+    if not os.path.isfile(#path) :
         print("---------------------------------------------------")
         print('Scraping followers of:', row['name'],)
         next_cursor = -1
@@ -37,7 +37,7 @@ for index,row in influential_user_list.iterrows():
                     time.sleep(remainder)
                     twitter_final = Twython(API_Key, API_Secret,Access_Token, Access_Token_Secret)
                 continue
-        with open('influential_user_follower/'+ str(row['Rank']) + "_" + row['name'] + '/followers_ids.txt') as f:
+        with open(#path) as f:
             for id in followers_list:
                 print(id,file=f)
             f.close()
