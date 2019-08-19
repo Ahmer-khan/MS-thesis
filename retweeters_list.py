82822018289f74e144b068b3acf937bf17d71f28
Ahmer Latif Khan <54271020+Ahmer-khan@users.noreply.github.com>
2019-08-19 16:54:34 +0500

    Update retweeters_list.py

--- retweeters_list.py
+++ retweeters_list.py
@@ -1,15 +1,15 @@
 import os
 trends = []
 path = ""
-for(path, dir, files) in os.walk("second_half_political2"):
+for(path, dir, files) in os.walk(#path):
     trends.extend(dir)
     break
 for trend in trends:
         #new_dir = os.path.join(path, name)
         #file = os.listdir(new_dir)
-        #retweets_file = open(new_dir + "/user_no_retweet.txt", 'w+')
+        #retweets_file = open(path)
         #retweets_file.close()
-        tweets = open(path + "/" + trend + "/retweets.txt","r")
+        tweets = open(#path)
         retweeters_list = []
         for tweet in tweets:
             tweet = tweet.replace("{","")
@@ -26,7 +26,7 @@ for trend in trends:
             for id in retweeters:
                 if not id in retweeters_list :
                     retweeters_list.append(id)
-                    with open(path + "/" + trend + "/unique_retweeters.txt","a") as f:
+                    with open(#path) as f:
                         print(id, file=f)
                 f.close()
         tweets.close()
